414db158dbaa17b1cf81e7be4ed514cd1721df0e
Samuel Que <szzzygthckd@gmail.com>
2021-10-25 12:13:07 +1300

    initial commit

---
+++ Howick_Interview/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Howick_Interview
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CW_input = new System.Windows.Forms.TextBox();
+            this.vel_input = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.vel_label = new System.Windows.Forms.Label();
+            this.state_label = new System.Windows.Forms.Label();
+            this.SW_label = new System.Windows.Forms.Label();
+            this.FR_Active = new System.Windows.Forms.Button();
+            this.FR_complete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Control Word";
+            // 
+            // CW_input
+            // 
+            this.CW_input.Location = new System.Drawing.Point(152, 91);
+            this.CW_input.MaxLength = 16;
+            this.CW_input.Name = "CW_input";
+            this.CW_input.Size = new System.Drawing.Size(164, 23);
+            this.CW_input.TabIndex = 1;
+            this.CW_input.TextChanged += new System.EventHandler(this.CW_input_TextChanged);
+            // 
+            // vel_input
+            // 
+            this.vel_input.Location = new System.Drawing.Point(152, 130);
+            this.vel_input.MaxLength = 16;
+            this.vel_input.Name = "vel_input";
+            this.vel_input.Size = new System.Drawing.Size(164, 23);
+            this.vel_input.TabIndex = 1;
+            this.vel_input.TextChanged += new System.EventHandler(this.vel_input_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Motor Target Velocity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Motor Actual Velocity";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Status Word";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(96, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "State";
+            // 
+            // vel_label
+            // 
+            this.vel_label.AutoSize = true;
+            this.vel_label.Location = new System.Drawing.Point(152, 171);
+            this.vel_label.Name = "vel_label";
+            this.vel_label.Size = new System.Drawing.Size(0, 15);
+            this.vel_label.TabIndex = 6;
+            // 
+            // state_label
+            // 
+            this.state_label.AutoSize = true;
+            this.state_label.Location = new System.Drawing.Point(152, 243);
+            this.state_label.Name = "state_label";
+            this.state_label.Size = new System.Drawing.Size(0, 15);
+            this.state_label.TabIndex = 7;
+            // 
+            // SW_label
+            // 
+            this.SW_label.AutoSize = true;
+            this.SW_label.Location = new System.Drawing.Point(152, 209);
+            this.SW_label.Name = "SW_label";
+            this.SW_label.Size = new System.Drawing.Size(0, 15);
+            this.SW_label.TabIndex = 8;
+            // 
+            // FR_Active
+            // 
+            this.FR_Active.Location = new System.Drawing.Point(58, 287);
+            this.FR_Active.Name = "FR_Active";
+            this.FR_Active.Size = new System.Drawing.Size(123, 23);
+            this.FR_Active.TabIndex = 9;
+            this.FR_Active.Text = "Fault Reaction";
+            this.FR_Active.UseVisualStyleBackColor = true;
+            this.FR_Active.Click += new System.EventHandler(this.FR_Active_Click);
+            // 
+            // FR_complete
+            // 
+            this.FR_complete.Location = new System.Drawing.Point(219, 287);
+            this.FR_complete.Name = "FR_complete";
+            this.FR_complete.Size = new System.Drawing.Size(163, 23);
+            this.FR_complete.TabIndex = 10;
+            this.FR_complete.Text = "Fault Reaction Complete";
+            this.FR_complete.UseVisualStyleBackColor = true;
+            this.FR_complete.Click += new System.EventHandler(this.FR_complete_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(660, 340);
+            this.Controls.Add(this.FR_complete);
+            this.Controls.Add(this.FR_Active);
+            this.Controls.Add(this.SW_label);
+            this.Controls.Add(this.state_label);
+            this.Controls.Add(this.vel_label);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CW_input);
+            this.Controls.Add(this.vel_input);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox CW_input;
+        private System.Windows.Forms.TextBox vel_input;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label vel_label;
+        private System.Windows.Forms.Label State;
+        private System.Windows.Forms.Label state_label;
+        private System.Windows.Forms.Label SW_label;
+        private System.Windows.Forms.Button FR_Active;
+        private System.Windows.Forms.Button FR_complete;
+    }
+}
+
---
+++ Howick_Interview/Form1.cs
@@ -0,0 +1,491 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Howick_Interview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Global.machineState.initialize();
+            Global.machineState.do_action();
+            print_labels(vel_label, state_label, SW_label);
+
+        }
+
+
+
+        public interface IState
+        {
+            IState compare_control(MachineState machineState);
+            void do_action(MachineState machineState);
+            IState fault_react(MachineState machineState);
+        }
+        public class Global
+        {
+            public static Regex regex = new Regex("[0-1]{16}");
+            public static UInt16 CW;
+            public static MachineState machineState = new MachineState();
+            public static UInt16 vel_desired;
+            public static UInt16 vel_out;
+            public static UInt16 sw_lbl;
+            public static string st_lbl;
+            public static bool fault_flag = false;
+        }
+
+        public class MachineState
+        {
+            private IState currentState;
+            public IState State
+            {
+                set { currentState = value; }
+            }
+            private ushort statusWord;
+            public ushort StatusWord
+            {
+                get { return statusWord; }
+                set { statusWord = value; }
+            }
+            public void initialize()
+            {
+                currentState = startState;
+                Global.machineState.compare_control();
+                Global.machineState.compare_control();
+            }
+            public void compare_control()
+            {
+                currentState = currentState.compare_control(this);
+            }
+
+            public void do_action()
+            {
+                currentState.do_action(this);
+            }
+
+            public void fault_react()
+            {
+                currentState.fault_react(this);
+            }
+
+            public StartState startState = new StartState();
+            public NotReadyOn notReadyOn = new NotReadyOn();
+            public SwOnDisabled swOnDisabled = new SwOnDisabled();
+            public ReadySwOn readySwOn = new ReadySwOn();
+            public SwOn swOn = new SwOn();
+            public OpEnabled opEnabled = new OpEnabled();
+            public QckStopActive qckStopActive = new QckStopActive();
+            public FaultRActive faultRActive = new FaultRActive();
+            public Fault fault = new Fault();
+
+        }
+
+        public class StartState : IState
+        {
+            
+            public IState compare_control(MachineState machineState)
+            {
+            
+                if (true)
+                {
+                    // Next State
+                    Console.WriteLine("Power-on, self-initialization");
+                    machineState.StatusWord = 0b0000000000000000;
+                    return machineState.notReadyOn;
+                    
+                }
+            }
+            public void do_action(MachineState machineState) { }
+            public IState fault_react(MachineState machineState)
+            {
+                return machineState.faultRActive;
+            }
+
+        }
+
+        public class NotReadyOn : IState
+        {
+            public IState compare_control(MachineState machineState)
+            {
+                if (true)
+                {
+                    Console.WriteLine("Initialized Successfully");
+                    machineState.StatusWord = 0b0000000001000000;
+                    return machineState.swOnDisabled;
+                    
+                }
+                
+            }
+            public void do_action(MachineState machineState)
+            {
+                Global.vel_out = 0;
+                Global.sw_lbl = machineState.StatusWord;
+                Global.st_lbl = "Not Ready to Switch On";
+            }
+            public IState fault_react(MachineState machineState)
+            {
+                return machineState.faultRActive;
+            }
+        }
+
+        public class SwOnDisabled : IState
+        {
+            public IState compare_control(MachineState machineState)
+            {
+                if (Global.fault_flag)
+                {
+                    return machineState.faultRActive;
+                }
+                UInt16 mask = 0b0000000000000111;
+                if ((Global.CW & mask) == 0b0000000000000110)
+                {
+                    Console.WriteLine("Ready To Switch On");
+                    machineState.StatusWord = 0b0000000000010001;
+                    return machineState.readySwOn;
+
+                }
+                else
+                {
+                    return machineState.swOnDisabled;
+                }
+            }
+            public void do_action(MachineState machineState)
+            {
+                Global.vel_out = 0;
+                Global.sw_lbl = machineState.StatusWord;
+                Global.st_lbl = "Switch On Disabled";
+            }
+            public IState fault_react(MachineState machineState)
+            {
+                return machineState.faultRActive;
+            }
+        }
+
+        public class ReadySwOn : IState
+        {
+            public IState compare_control(MachineState machineState)
+            {
+                if (Global.fault_flag)
+                {
+                    return machineState.faultRActive;
+                }
+                UInt16 mask = 0b0000000000000111;
+                UInt16 mask2 = 0b0000000000000110;
+                UInt16 mask3 = 0b0000000000000010;
+                if ((Global.CW & mask) == 0b0000000000000111)
+                {
+                    Console.WriteLine("Switched On");
+                    machineState.StatusWord = 0b0000000000100011;
+                    return machineState.swOn;
+
+                }
+                else if ((Global.CW & mask2) == 0b0000000000000010 || (Global.CW & mask3) == 0b0000000000000000)
+                {
+                    Console.WriteLine("Switch On Disabled");
+                    machineState.StatusWord = 0b0000000001000000;
+                    return machineState.swOnDisabled;
+                }
+                else
+                {
+                    return machineState.readySwOn;
+                }
+            }
+            public void do_action(MachineState machineState)
+            {
+                Global.vel_out = 0;
+                Global.sw_lbl = machineState.StatusWord;
+                Global.st_lbl = "Ready to Switch On";
+            }
+            public IState fault_react(MachineState machineState)
+            {
+                return machineState.faultRActive;
+            }
+        }
+
+        public class SwOn : IState
+        {
+            public IState compare_control(MachineState machineState)
+            {
+                if (Global.fault_flag)
+                {
+                    return machineState.faultRActive;
+                }
+                UInt16 mask = 0b0000000000000110;
+                UInt16 mask2 = 0b0000000000001111;
+                UInt16 mask3 = 0b0000000000000110;
+                UInt16 mask4 = 0b0000000000000010;
+                if ((Global.CW & mask2) == mask2)
+                {
+                    Console.WriteLine("Operation Enabled");
+                    machineState.StatusWord = 0b0000000000100111;
+                    return machineState.opEnabled;
+
+                }
+                else if ((Global.CW & mask3) == 0b0000000000000010 || (Global.CW & mask4) == 0b0000000000000000)
+                {
+                    Console.WriteLine("Switch On Disabled");
+                    machineState.StatusWord = 0b0000000001000000;
+                    return machineState.swOnDisabled;
+                }
+                else if ((Global.CW & mask)  == 0b0000000000000110)
+                {
+                    Console.WriteLine("Ready to Switch On");
+                    machineState.StatusWord = 0b0000000000100001;
+                    return machineState.readySwOn;
+                }
+
+                else
+                {
+                    return machineState.swOn;
+                }
+            }
+            public void do_action(MachineState machineState)
+            {
+                Global.vel_out = 0;
+                Global.sw_lbl = machineState.StatusWord;
+                Global.st_lbl = "Switched On";
+            }
+            public IState fault_react(MachineState machineState)
+            {
+                return machineState.faultRActive;
+            }
+        }
+
+        public class OpEnabled : IState
+        {
+            public IState compare_control(MachineState machineState)
+            {
+                if (Global.fault_flag)
+                {
+                    return machineState.faultRActive;
+                }
+
+                UInt16 mask = 0b0000000000000010;
+                UInt16 mask2 = 0b0000000000001111;
+                UInt16 mask3 = 0b0000000000000110;
+                if ((Global.CW & mask) == 0)
+                {
+                    Console.WriteLine("Switch On Disabled");
+                    machineState.StatusWord = 0b0000000001000000;
+                    return machineState.swOnDisabled;
+
+                }
+                else if ((Global.CW & mask3) == 0b0000000000000010)
+                {
+                    Console.WriteLine("Quick Stop Active");
+                    machineState.StatusWord = 0b0000000000000111;
+                    return machineState.qckStopActive;
+                }
+                else if ((Global.CW & mask2) == mask2 >> 1)
+                {
+                    Console.WriteLine("Switched On");
+                    machineState.StatusWord = 0b0000000000100011;
+                    return machineState.swOn;
+                }
+
+                else
+                {
+                    return machineState.opEnabled;
+                }
+            }
+            public void do_action(MachineState machineState) 
+            {
+                Global.vel_out = Global.vel_desired;
+                Global.sw_lbl = machineState.StatusWord;
+                Global.st_lbl = "Operation Enabled";
+            }
+            public IState fault_react(MachineState machineState)
+            {
+                return machineState.faultRActive;
+            }
+        }
+
+
+        public class QckStopActive : IState
+        {
+            public IState compare_control(MachineState machineState)
+            {
+                if (Global.fault_flag)
+                {
+                    return machineState.faultRActive;
+                }
+                UInt16 mask = 0b0000000000000010;
+                if ((Global.CW & mask) == 0)
+                {
+                    Console.WriteLine("Switch On Disabled");
+                    machineState.StatusWord = 0b0000000001000000;
+                    return machineState.swOnDisabled;
+
+                }
+                else
+                {
+                    return machineState.qckStopActive;
+                }
+            }
+            public void do_action(MachineState machineState)
+            {
+                Global.vel_out = 0;
+                Global.sw_lbl = machineState.StatusWord;
+                Global.st_lbl = "Quick Stop Active";
+            }
+            public IState fault_react(MachineState machineState)
+            {
+                return machineState.faultRActive;
+            }
+        }
+
+        public class FaultRActive : IState
+        {
+            public IState compare_control(MachineState machineState)
+            {
+                if (Global.fault_flag)
+                {
+                    return machineState.faultRActive;
+                }
+                else
+                {
+                    Console.WriteLine("correct");
+                    machineState.StatusWord = 0b0000000000001000;
+                    return machineState.fault;
+                }
+                
+            }
+            public void do_action(MachineState machineState)
+            {
+                machineState.StatusWord = 0b0000000000001111;
+                Global.vel_out = 0;
+                Global.sw_lbl = machineState.StatusWord;
+                Global.st_lbl = "Fault Reaction Active";
+            }
+            public IState fault_react(MachineState machineState)
+            {
+                if (!Global.fault_flag)
+                {
+                    return machineState.fault;
+                }
+                return machineState.faultRActive;
+            }
+        }
+
+        public class Fault : IState
+        {
+            public IState compare_control(MachineState machineState)
+            {
+                if (Global.fault_flag)
+                {
+                    return machineState.faultRActive;
+                }
+                UInt16 mask = 0b0000000010000000;
+                if ((Global.CW & mask) == mask)
+                {
+                    machineState.StatusWord = 0b0000000001000000;
+                    return machineState.swOnDisabled;
+
+                }
+                else
+                {
+                    return machineState.fault;
+                }
+            }
+            public void do_action(MachineState machineState)
+            {
+                Global.vel_out = 0;
+                Global.sw_lbl = machineState.StatusWord;
+                Global.st_lbl = "Fault";
+            }
+            public IState fault_react(MachineState machineState)
+            {
+                    return machineState.faultRActive;
+                
+            }
+        }
+
+        //--------------------------------------------------------------------------//
+        private void print_labels(Label vel_lbl, Label st_label, Label sw_label)
+        {
+            //vel_lbl.Text = Convert.ToString(Global.vel_out, 2);
+            st_label.Text = Global.st_lbl;
+            //sw_label.Text = Convert.ToString(Global.sw_lbl, 2); 
+            vel_lbl.Text = converting_labels(Global.vel_out);
+            sw_label.Text = converting_labels(Global.sw_lbl);
+        }
+
+
+        public String converting_labels(UInt16 value)
+        {
+            String binaryString = Convert.ToString(value, 2);
+            return binaryString.PadLeft(16, '0');
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void CW_input_TextChanged(object sender, EventArgs e)
+        {
+            // Extracting Control Word
+            Global.machineState.do_action();
+            if (Global.regex.IsMatch(CW_input.Text))
+            {
+                Global.CW = Convert.ToUInt16(CW_input.Text, 2);
+            }
+            Global.machineState.compare_control();
+            Global.machineState.do_action();
+            print_labels(vel_label, state_label, SW_label);
+        }
+
+        private void CW_input_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+            
+        }
+
+        private void vel_input_TextChanged(object sender, EventArgs e)
+        {
+            // Extracting Desired Motor Velocity
+            Global.machineState.do_action();
+            if (Global.regex.IsMatch(vel_input.Text))
+            {
+                Global.vel_desired = Convert.ToUInt16(vel_input.Text, 2);
+            }
+            Global.machineState.compare_control();
+            Global.machineState.do_action();
+            print_labels(vel_label, state_label, SW_label);
+        }
+
+        private void FR_Active_Click(object sender, EventArgs e)
+        {
+            // Fault Reaction Active buttion click -> move to FR state
+            Global.fault_flag = true;
+            Global.machineState.compare_control();
+            Global.machineState.do_action();
+            print_labels(vel_label, state_label, SW_label);
+
+        }
+
+        private void FR_complete_Click(object sender, EventArgs e)
+        {
+            // Fault Reaction Complete button click -> move to Fault State
+            Global.fault_flag = false;
+            Global.machineState.compare_control();
+            Global.machineState.do_action();
+            print_labels(vel_label, state_label, SW_label);
+        }
+
+    }
+
+}
+
+
+
+
+
+
